6d0950a966a15714b29c6130c3225d4786f3e630
DeeBest <simphiwedladla8@gmail.com>
2024-04-25 23:33:57 +0200

    added function for iterating over the careers array and display each career characters

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 const footerParagraph = document.getElementById('footer-paragraph');
 const year = new Date().getFullYear();
 footerParagraph.textContent = `Created By \xA9Simphiwe ${year}`;
+
+const main = document.getElementById('main');
+
+const careers = ['Web Developer', 'Freelancer', 'Forex Trader', 'Truck Driver'];
+
+let careerIndex = 0;
+let characterIndex = 0;
+
+updateText();
+
+function updateText() {
+  characterIndex++;
+  main.innerHTML = `<h2>I am a ${careers[careerIndex].slice(
+    0,
+    characterIndex
+  )}</h2>`;
+
+  if (characterIndex === careers[careerIndex].length) {
+    careerIndex++;
+    characterIndex = 0;
+  }
+  if (careerIndex === careers.length) {
+    careerIndex = 0;
+  }
+  setTimeout(updateText, 400);
+}
